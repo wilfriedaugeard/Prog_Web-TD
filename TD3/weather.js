44a9a249d70834aa896b4757d33003d896a9fe80
wilfried.augeard@etu.u-bordeaux.fr <wilfried.augeard@etu.u-bordeaux.fr>
2020-09-20 12:26:02 +0200

    Add more info

--- TD3/weather.js
+++ TD3/weather.js
@@ -6,10 +6,12 @@ let data = null;
 function sendMeteoRequest(){
     const cityValue = document.getElementById('inputCity').value;
     if(cityValue == "" || cityValue == null){
-        document.getElementById("data").innerHTML = "Please enter a city name";
+        console.log("ok");
+        console.log(cityValue);
+        document.getElementById("cityName").innerHTML = "Please enter a city name";
         return;
     }
-    
+    console.log(cityValue);
     request.open('GET', requestURL.concat(cityValue));
     request.responseType = 'json';
     request.send();
@@ -18,7 +20,7 @@ function sendMeteoRequest(){
         const jsonObj = request.response;
         const strObj = JSON.stringify(jsonObj);
         data = JSON.parse(strObj);
-        document.getElementById('currentInfo').style.display = "block";
+        document.getElementById('data').style.display = "block";
         getCityName();
         displayTodayWeather();
     }
@@ -29,17 +31,53 @@ function getCityName(){
     console.log(data);
 }
 
+function getWindDirection(){
+    const dir = data.current_condition.wnd_dir;
+    let fullDir = "";
+    switch(dir){
+        case 'N':
+            fullDir = "Nord";
+            break;
+        case 'E':
+            fullDir = "Est"
+            break;
+        case 'O':
+            fullDir = "Ouest";
+            break;
+        case 'S':
+            fullDir = "Sud";
+            break;
+        case 'NE':
+            fullDir = "Nord-Est";
+            break;
+        case 'NO':
+            fullDir = "Nord-Ouest";
+            break;
+        case 'SE':
+            fullDir = "Sud-Est";
+            break;
+        case 'SO':
+            fullDir = "Sud-Ouest";
+            break;
+        default:
+            fullDir = "Inconnu";
+    }
+    document.getElementById('windDir').innerHTML = "direction: "+fullDir;
+    
+}
+
 function displayTodayWeather(){
     document.getElementById('todayName').innerHTML = data.current_condition.date
     document.getElementById('hourName').innerHTML = data.current_condition.hour;
     if(document.getElementById('imgWeather').childElementCount == 0){
         const img = document.createElement('img'); 
-        img.src =  data.current_condition.icon;
+        img.src =  data.current_condition.icon_big;
         document.getElementById('imgWeather').appendChild(img); 
     }
     document.getElementById('conditionName').innerHTML = data.current_condition.condition;
     document.getElementById('currentDeg').innerHTML = data.current_condition.tmp;
     document.getElementById('minTmp').innerHTML = "min: "+data.fcst_day_0.tmin+"°";
     document.getElementById('maxTmp').innerHTML = "max: "+data.fcst_day_0.tmax+"°";
-    document.getElementById('wind').innerHTML = "Vent: "+data.current_condition.wnd_spd+"km/h";
+    document.getElementById('wind').innerHTML = "vent: "+data.current_condition.wnd_spd+"km/h";
+    getWindDirection();
 }
